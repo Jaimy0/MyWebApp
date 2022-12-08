edd8b8f44d82948dfd016029308f7fbb6c1a1f8e
Jaimy0 <jaimy.vahl@student.hu.nl>
2022-12-08 13:28:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 echo '<h1>';
 echo 'Het IP van de loadbalancer is: ' . $_SERVER['SERVER_NAME'] . '<br>';
-echo 'Het IP van de webserver is: ' . $_SERVER['SERVER_ADDR'] . '<br>';
+echo 'Het private IP van de webserver is: ' . $_SERVER['SERVER_ADDR'] . '<br>';
 echo 'De hostname is van de webserver is: ' . gethostname();
 echo '</h1>';
 ?>
