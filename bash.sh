2db0c457d96f6d7dd1fc3f21fb7d84c8da0d61ad
Jaimy0 <jaimy.vahl@student.hu.nl>
2022-12-08 11:02:46 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -3,6 +3,8 @@ sudo apt update -y
 sudo apt install apache2 -y
 sudo apt install php libapache2-mod-php php-mysql -y
 git clone https://github.com/Jaimy0/MyWebApp
-sudo cat MyWebApp/dir.conf > /etc/apache2/mods-enabled/dir.conf
+sudo chmod 777 MyWebApp/dir.conf
+sudo chown root.root MyWebApp/dir.conf
+sudo mv MyWebApp/dir.conf /etc/apache2/mods-enabled/dir.conf
 sudo mv MyWebApp/index.php /var/www/html/index.php
 sudo systemctl reload apache2
